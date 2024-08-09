d0ce4d7842487e0a48e628f13f283d20cb3e6bf0
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-09 16:54:47 +0545

    added slider styled

--- src/components/slider/BeforeAfterSlider.js
+++ src/components/slider/BeforeAfterSlider.js
@@ -1,16 +1,23 @@
+import React, { useState, useRef, useEffect } from "react";
 import "./BeforeAfterSlider.css";
 
 export default function BeforeAfterSlider() {
+  const [sliderPosition, setSliderPosition] = useState(50);
+  const containerRef = useRef(null);
+
+  const handleSliderChange = (e) => {
+    setSliderPosition(e.target.value);
+  };
+
   return (
-    <div className="container">
-      <div className="before"></div>
+    <div className="container" ref={containerRef}>
+      <div className="before" style={{ width: `${sliderPosition}%` }}></div>
       <div className="after"></div>
       <input
         type="range"
-        // min="1"
-        // max="100"
-        // value="50"
         className="slider"
+        value={sliderPosition}
+        onChange={handleSliderChange}
       ></input>
     </div>
   );
